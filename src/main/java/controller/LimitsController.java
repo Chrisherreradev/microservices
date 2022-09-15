f1508cf96efc48dfb21085f51408570974d45ae2
Christian Herrera <christianherrera@live.com.mx>
2022-09-14 23:13:47 -0500

    get limit values from configuration app.prop

--- src/main/java/controller/LimitsController.java
+++ src/main/java/controller/LimitsController.java
@@ -1,13 +1,21 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import bean.Limits;
+import configuration.Configuration;
 
+@RestController
 public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
 	
 	@GetMapping("/limits")
 	public Limits retrieveLimits() {
-		return new Limits(1, 1000);
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+		//return new Limits(1, 1000);
 	}
 }
